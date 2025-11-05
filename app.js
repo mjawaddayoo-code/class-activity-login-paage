f8f57f1d2145c2c01e6561dbcdd9a03925c8c264
muhammad jawad <mjawaddayoo@gmail.com>
2025-11-05 19:56:46 +0500

    add:add js file

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log("Hello");
+// alert("Teasting alert");
+// var isPositive = false;
+
+// var number = -12;
+// if (number % 2 === 0) {
+//     alert("Number is Even number");
+// }
+// else{
+//     alert("number is  odd Number");
+// }
+const students = ['jawad','usman', 'hammad'];
+console.table(students);
